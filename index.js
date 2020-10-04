44500527fb82788ddf7414ef2d3594bf1f4fb120
omarnabi1 <onabi20@gmail.com>
2020-10-04 00:27:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(people,event){
+    let thankYouCards = [];
+    for(let i = 0; i < people.length; i++){
+       thankYouCards.push( `Thank you, ${people[i]}, for the wonderful ${event} gift!` );
+    }
+    return thankYouCards;
+}
+
+
+
+
+
+function countDown(time){
+    while(time > 0){
+        console.log(time)
+        time--;
+    }
+    console.log(time)
+}
